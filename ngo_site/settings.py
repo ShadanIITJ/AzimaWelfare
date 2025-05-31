a42dc68bbb0622bba1bc5a86b12c8faa0c9ea9da
ShadanIITJ <shadan.zahid123@gmail.com>
2025-06-01 03:44:48 +0530

    setting

--- ngo_site/settings.py
+++ ngo_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-li%bwp5l#f4#gm$sj@4ie&u4%w12&fqpc-zem#kz0y2hu&ve+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')                                                                                            
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (for event images, etc.)
